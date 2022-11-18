0ea0d54c6354d21f5ffbc4c8f0f0960705837c68
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-11-18 10:36:28 -0500

    Add: State with dummy data and render with map

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,28 @@
+import { useState } from 'react';
 import PageLayout from '../components/PageLayout';
 
 export default function Home() {
+  const [notes, setNotes] = useState([
+    { id: 1, text: 'This is my first note', createdAt: '2022-11-18 10:23 AM' },
+    { id: 2, text: 'This is my second note', createdAt: '2022-11-18 10:34 AM' },
+  ]);
+
   return (
     <PageLayout metaTitle='QuickNotes | Home'>
       <section className='mx-6 pt-6 md:mx-0'>
         <ul className='flex flex-col gap-6' role='list'>
-          <li>
-            <h2 className='text-2xl font-bold'>2022-11-18 10:23 AM</h2>
-            <div className='flex flex-row justify-between'>
-              <p className='text-gray-600'>This is a note</p>
-              <div className='flex flex-row space-x-4'>
-                <a href='#'>Edit</a>
-                <button>Delete</button>
+          {notes.map((note) => (
+            <li key={note.id}>
+              <h2 className='text-2xl font-bold'>{note.createdAt}</h2>
+              <div className='flex flex-row justify-between'>
+                <p className='text-gray-600'>{note.text}</p>
+                <div className='flex flex-row space-x-4'>
+                  <a href='#'>Edit</a>
+                  <button>Delete</button>
+                </div>
               </div>
-            </div>
-          </li>
-          <li>
-            <h2 className='text-2xl font-bold'>2022-11-18 10:27 AM</h2>
-            <div className='flex flex-row justify-between'>
-              <p className='text-gray-600'>This is another note</p>
-              <div className='flex flex-row space-x-4'>
-                <a href='#'>Edit</a>
-                <button>Delete</button>
-              </div>
-            </div>
-          </li>
+            </li>
+          ))}
         </ul>
       </section>
     </PageLayout>
