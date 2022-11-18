0fffea6796f935b9691651f39f496d79cf294050
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-11-18 11:28:36 -0500

    Change: refactor notesList and note to their components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import NotesList from '../components/Noteslist';
 import PageLayout from '../components/PageLayout';
 import useNotes from '../hooks/useNotes';
 
@@ -6,20 +7,7 @@ export default function Home() {
   return (
     <PageLayout metaTitle='QuickNotes | Home'>
       <section className='mx-6 pt-6 md:mx-0'>
-        <ul className='flex flex-col gap-6' role='list'>
-          {notes.map((note) => (
-            <li key={note.id}>
-              <h2 className='text-2xl font-bold'>{note.createdAt}</h2>
-              <div className='flex flex-row justify-between'>
-                <p className='text-gray-600'>{note.text}</p>
-                <div className='flex flex-row space-x-4'>
-                  <a href='#'>Edit</a>
-                  <button>Delete</button>
-                </div>
-              </div>
-            </li>
-          ))}
-        </ul>
+        <NotesList notes={notes} />
       </section>
     </PageLayout>
   );
