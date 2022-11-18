9ac41d2a86cbf5d76c9af4955322577a2bf314be
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-11-18 11:05:17 -0500

    Remove: umport useContext statement in noteContext

--- context/noteContext.tsx
+++ context/noteContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useState } from 'react';
 import type INote from '../types/note';
 import type { INoteContext, INoteContextProps } from '../types/note';
 
